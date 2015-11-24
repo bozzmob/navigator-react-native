79521f31bb2d5b283be3b73778072b3d5d8f18b2
satvik <satvik.jagannath7@gmail.com>
2015-11-24 23:54:27 +0530

    Navigator component

--- index.android.js
+++ index.android.js
@@ -8,26 +8,98 @@ var React = require('react-native');
 var {
   AppRegistry,
   StyleSheet,
-  Text,
+  Navigator,
+  ToolbarAndroid,
   View,
+  Text,
+  TouchableOpacity,
+  BackAndroid
 } = React;
 
-var NavigatorReactNative = React.createClass({
-  render: function() {
+var PostsView = require('./App/Views/Posts');
+var PostView = require('./App/Views/Post');
+var WebView = require('./App/Views/Web');
+var _navigator;
+
+
+var NavToolbar = React.createClass({
+
+  componentWillMount: function() {
+    var navigator = this.props.navigator;
+  },
+
+  render: function () {
+    if (this.props.navIcon) {
+      return (
+        <ToolbarAndroid
+          style={styles.toolbar}
+          navIcon={{uri: "ic_arrow_back_white_24dp", isStatic: true}}
+          onIconClicked={this.props.navigator.pop}
+          titleColor="#ffffff"
+          title='Hacker News - Top Stories' />
+      )
+    }
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+      <ToolbarAndroid
+        style={styles.toolbar}
+        onIconClicked={this.props.navigator.pop}
+        titleColor="#ffffff"
+        title='Hacker News - Top Stories' />
+    )
   }
+});
+
+BackAndroid.addEventListener('hardwareBackPress', () => {
+  if (_navigator.getCurrentRoutes().length === 1  ) {
+     return false;
+  }
+  console.log("_navigator = "+_navigator);
+  _navigator.pop();
+  return true;
+});
+
+var NavigatorReactNative = React.createClass({
+
+  renderScene: function(route, navigator) {
+    _navigator = navigator;
+    if (route.id === 'Home') {
+      console.log("Home - route= "+JSON.stringify(route)+" navigator= "+navigator+" route.index= "+JSON.stringify(route.index)+" route.post= "+JSON.stringify(route.post));
+      return (
+        <View style={{flex: 1}}>
+        <NavToolbar navigator={navigator}/>
+        <PostsView nav = {navigator} name = { route.name }/>
+        </View>
+      );
+    }
+    if (route.id === 'Post') {
+      console.log("Post - route= "+JSON.stringify(route)+" navigator= "+navigator+" route.index= "+JSON.stringify(route.index)+" route.post= "+JSON.stringify(route.post));
+      return (
+        <View style={{flex: 1}}>
+        <NavToolbar navIcon={true} navigator={navigator}/>
+        <PostView index = {route.index} post={route.post} nav={navigator}  />
+        </View>
+      )
+    }
+
+    if (route.id === 'WebView') {
+      console.log("WebView - route= "+JSON.stringify(route)+" navigator= "+navigator+" route.index= "+JSON.stringify(route.index)+" route.post= "+JSON.stringify(route.post));
+      return (
+        <View style={{flex: 1}}>
+            <NavToolbar navIcon={true} navigator={navigator}/>
+            <WebView index = {route.index + 1}  title={route.title} url={route.url} />
+          </View>
+      )
+    }
+  },
+  render: function() {
+    //console.log("WebView - route= "+route+" navigator= "+navigator+" route.index= "+route.index+" route.post= "+route.post);
+    return (
+      <Navigator
+      initialRoute = {{id: 'Home', index: 0}}
+      configureScene={() => Navigator.SceneConfigs.FadeAndroid}
+      renderScene={this.renderScene} />
+    );
+  },
 });
 
 var styles = StyleSheet.create({
